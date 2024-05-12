eb9769907b73d53c208f9964c99a1db69aa696c2
ELMEHDI ACHAHED <117928790+Trpsky@users.noreply.github.com>
2024-05-12 14:36:50 +0100

    refactor : Refactor GUI and use OS for image management  :100:

--- script.py
+++ script.py
@@ -1,19 +1,18 @@
-import tkinter as tk
-from tkinter import filedialog
 from rembg import remove
+import os
 from PIL import Image
-import io
 
-def open_image():
-    file_path = filedialog.askopenfilename()
+def process_image(file_path):
     with open(file_path, 'rb') as img_file:
         img = img_file.read()
     output = remove(img)
-    result = Image.open(io.BytesIO(output))
-    result.show()
 
-root = tk.Tk()
-button = tk.Button(root, text="Open Image", command=open_image)
-button.pack()
+    base_name = os.path.basename(file_path)
+    name, _ = os.path.splitext(base_name)
+    result_name = f"{name}_output_bg_remover_trpsky.png"  # Always use .png extension
 
-root.mainloop()
+    result_path = os.path.join(os.path.dirname(file_path), result_name)
+    with open(result_path, 'wb') as f:
+        f.write(output)
+
+    return output, result_path
