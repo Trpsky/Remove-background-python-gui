162e002b9f1b75bf03a7d783c596f36386ba566a
ELMEHDI ACHAHED <117928790+Trpsky@users.noreply.github.com>
2024-05-12 14:39:41 +0100

    feat: Create GUI in new gui.py file :8ball:
    Move all GUI-related code to a new file called gui.py. This separates the GUI code from the image processing code, making the codebase easier to maintain and understand.

---
+++ gui.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from tkinter import filedialog
+from ttkthemes import ThemedTk
+from PIL import Image, ImageTk
+import os
+import script
+
+def open_image():
+    file_path = filedialog.askopenfilename()
+    if not file_path:
+        return
+
+    output, result_path = script.process_image(file_path)
+    # picture original
+    original = Image.open(file_path)
+    original = original.resize((250, 250))
+    original_photo = ImageTk.PhotoImage(original)
+    original_label.config(image=original_photo)
+    original_label.image = original_photo
+
+    # picture result
+    result = Image.open(result_path)
+    result = result.resize((250, 250))
+    result_photo = ImageTk.PhotoImage(result)
+    result_label.config(image=result_photo)
+    result_label.image = result_photo
+
+    result_label.bind("<Button-1>", lambda e: os.system(f'start {result_path}'))
+
+root = ThemedTk(theme="arc")
+root.geometry('600x300')
+
+button = tk.Button(root, text="Open Image", command=open_image)
+button.pack()
+
+original_label = tk.Label(root)
+original_label.pack(side="left", padx=10, pady=10)
+
+result_label = tk.Label(root)
+result_label.pack(side="right", padx=10, pady=10)
+
+root.mainloop()
